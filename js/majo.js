2fcbc8bb0a9b9e2c42b7c7b7787dbe5655da2a87
majo-kanazawa institute of technology <102805169+majo-kanazawa@users.noreply.github.com>
2022-05-09 13:03:14 +0900

    Update majo.js

--- js/majo.js
+++ js/majo.js
@@ -1,7 +1,7 @@
  $('.slider').slick({
     autoplay: true,//自動的に動き出すか。初期値はfalse。
     autoplaySpeed: 3500,//次のスライドに切り替わる待ち時間
-    speed:1000,//スライドの動きのスピード。初期値は300。
+    speed:1500,//スライドの動きのスピード。初期値は300。
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
     slidesToShow: 1,//スライドを画面に3枚見せる
     slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
